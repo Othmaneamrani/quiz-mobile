35977d9731e213fee374c623f8cda41c0b07548e
Othmaneamrani <othmaneamrani31@gmail.com>
2024-04-05 19:05:12 +0000

    details

--- app/src/main/java/com/example/quiz/Depart.java
+++ app/src/main/java/com/example/quiz/Depart.java
@@ -5,15 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Depart  extends AppCompatActivity {
 
     ImageView imageView,imageView2;
+    private TextView name;
     Button bStart;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference();
+String userAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +42,30 @@ public class Depart  extends AppCompatActivity {
         imageView = findViewById(R.id.couv);
         imageView2 = findViewById(R.id.brain);
         bStart = (Button) findViewById(R.id.bStart);
+        name= findViewById(R.id.name);
 
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        String userEmail = currentUser.getEmail();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference usersRef = db.collection("User");
+
+        usersRef.whereEqualTo("email", userEmail).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                                                  @Override
+                                                                                  public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                                                      if (task.isSuccessful()) {
+                                                                                          for (QueryDocumentSnapshot document : task.getResult()) {
+                                                                                              userAuth = String.valueOf(document.get("nom"));
+                                                                                              name.setText("Bienvenue "+userAuth);
+                                                                                          }
+                                                                                      }
+                                                                                      else {
+                                                                                          Toast.makeText(getApplicationContext(), "User introuvable.", Toast.LENGTH_SHORT).show();
+                                                                                      }
+                                                                                  }
+                                                                              }
+                                                                            );
 
 
         Glide.with(this)
